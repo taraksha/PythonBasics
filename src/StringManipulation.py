4147d0a7d7f64d00330f2a42c3c1df5514f3fdbf
taraksha <ashasumathi@gmail.com>
2025-08-13 16:17:10 +0900

    added formatting into a function

--- src/StringManipulation.py
+++ src/StringManipulation.py
@@ -1,6 +1,5 @@
-productName= "Milk"
-quantity=4
-price=200
-
-#formatting
-print(f" You Have ordered {quantity} {productName} for {quantity*price}")
+def formatting():
+    product_name = "Milk"
+    quantity = 4
+    price = 200
+    print(f" You Have ordered {quantity} {product_name} for {quantity*price}")
