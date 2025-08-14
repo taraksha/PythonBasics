821d19172819e72abfb6e59e4bc007755f5325a9
taraksha <ashasumathi@gmail.com>
2025-08-14 16:45:56 +0900

    Went through different datatypes

--- src/datatypes.py
+++ src/datatypes.py
@@ -1,4 +1,17 @@
 #scopes Local->Enclosing->Global->BuiltIn
-print(__file__)#built in variable, can be used anywhere
+print(__file__)#python built in variable, can be used anywhere
 
+a = 10
+b = 'Asha'
+c = "Name"
+d = "234"
+e = 10.2
+f = True #bool
 
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(int(d)))
+print(type(e))
+print(type(f))
