b8cbf3dd6cc127b52f119aeeb8ed688ab482243c
taraksha <ashasumathi@gmail.com>
2025-08-20 12:26:56 +0900

    Added the list of datatypes in comments

--- src/datatypes.py
+++ src/datatypes.py
@@ -15,3 +15,15 @@ print(type(d))
 print(type(int(d)))
 print(type(e))
 print(type(f))
+
+
+"""
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+"""
