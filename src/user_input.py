e3e2f6e7858c687218d969fef17da841f0be7a75
taraksha <ashasumathi@gmail.com>
2025-08-20 11:16:36 +0900

    Datatype casting

--- src/user_input.py
+++ src/user_input.py
@@ -7,10 +7,12 @@ import sys
 full_name = sys.argv[1]
 
 in command prompt
-python3 .\\src\\userInput.py Asha (note:use one \\ instead)
+python3 .\\src\\user_input.py Asha (note:use one \\ instead)
 
 """
 
+
+"""
 full_name = input("Enter your Full name: ") #Input from console
 
 email = full_name.lower().replace(" ",".") + "@companyname.com"
@@ -18,3 +20,10 @@ email = full_name.lower().replace(" ",".") + "@companyname.com"
 print("\n--- Your Profile ---")
 print( "Full Name: ", full_name)
 print("Email: "+email)
+"""
+
+#NOTE: Input is always string. use casting to change datatype
+a = input("Enter the first number:")
+#Handle error later here
+b = int(input("Enter the second number"))
+print("sum :"+str(a+b))
