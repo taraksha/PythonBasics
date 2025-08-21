62588e53ea769b4471314e32b045f34cd52dafe8
taraksha <ashasumathi@gmail.com>
2025-08-21 14:40:27 +0900

    Done logical and other operators

--- src/operators.py
+++ src/operators.py
@@ -1,6 +1,7 @@
-a=15
-b=4
-
+a = 15
+b = 4
+x = 6
+name = "Asha"
 print(a+b)
 print(a-b)
 print(a/b)
@@ -8,3 +9,11 @@ print(a*b)
 print(a%b)
 print(a**b) #15^4
 print(a//b) #round off/ floor
+
+x += 3
+print(x)
+
+print(a!=b)
+print(a is not b)
+print(a<30 and b>1)
+print("sha" in name)
