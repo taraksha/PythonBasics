cd587acb51d32f93cc2f9146fde68d865b6bc66b
taraksha <ashasumathi@gmail.com>
2025-08-21 14:40:49 +0900

    Added comments for bool

--- src/datatypes.py
+++ src/datatypes.py
@@ -7,6 +7,20 @@ c = "Name"
 d = "234"
 e = 10.2
 f = True #bool
+"""
+Almost any value is evaluated to True if it has some sort of content.
+
+Any string is True, except empty strings.
+
+Any number is True, except 0.
+
+Any list, tuple, set, and dictionary are True, except empty ones.
+
+In fact, there are not many values that evaluate to False, except empty values, such as (), [], {}, "",
+ the number 0, and the value None. And of course the value False evaluates to False.
+
+ojects are False if its __len__ is zero
+"""
 
 print(type(a))
 print(type(b))
@@ -15,6 +29,16 @@ print(type(d))
 print(type(int(d)))
 print(type(e))
 print(type(f))
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x,y,z)
+
+"""
+import random
+
+print(random.randrange(1, 10))
+
+"""
 
 
 """
