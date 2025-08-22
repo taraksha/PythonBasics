28fca334d5c85e85bb1455d9696ae500baacab6e
taraksha <ashasumathi@gmail.com>
2025-08-22 14:30:43 +0900

    Operations in List

--- src/datastructure.py
+++ src/datastructure.py
@@ -1,12 +1,20 @@
 #LIST uses [] ordered, mutable, allows duplicates, any datatype
+from typing import List
+
 mylist = ["apple", 1 , "cherry"]
-print(type(mylist))
-print(len(mylist))
-print(mylist[-1])
+mylist2 = list(["using","constructor"])
+
+print(mylist)
+print(type(mylist))#<class 'list'>
+
+print(len(mylist)) #3
+print(mylist[-1]) #cherry
 if "apple" in mylist:
     print("Yes, we have apple")
+
+#['apple', 1, 'cherry']
 mylist[1] = "banana" #update the list at the given position
-print(mylist)
+#['apple', 'banana', 'cherry']
 mylist[1:3] = ["blackcurrant", "watermelon"]  #update the list at the given positions
 print(mylist)
 mylist[1:2] = ["coconut", "chocolate"]  #update the given position alone
@@ -24,8 +32,51 @@ tropical = ["ExtendMango", "pineapple", "papaya"]
 mylist.extend(tropical) #can extend other datatypes too
 print(mylist)
 
+#Remove
+mylist.remove("Actual insert")
+print(mylist)
+mylist.pop() #removes last element
+print(mylist)
+mylist.pop(1) #removes last element
+print(mylist)
+del mylist[1]
+print(mylist)
+del mylist #deletes the list. cant be accessed after that. Throws error
+mylist2.clear() #clears the contents
+print(mylist2)
 
 
+#Loop
+list = []
+for i in range(5):
+    list.append(i) #create new list
+print(list)
+
+for i in list:
+    print(i)
+
+[print(x) for x in list]
+
+#Comprehention
+fruits = ["apple", "banana", "cherry", "kiwi", "mango","KIWI"]
+#newlist = [expression for item in iterable if condition == True]
+newfruits = [x.upper() for x in fruits if "a" in x]
+
+print(newfruits)
+
+editedfruits = [x if x != "banana" else "orange" for x in fruits]
+print(editedfruits)
+print("")
+#Sort
+sortlist = ["apple", "banana", "cherry", "kiwi", "mango","KIWI"]
+sortlist.sort() #sorts from Uppercase to lowercase
+print(sortlist)
+sortlist.sort(key = str.lower)
+print(sortlist)
+sortlist.sort(reverse=True)
+print(sortlist)
+sortlist.reverse()
+print(sortlist)
 #TUPLE is a collection which is ordered and unchangeable. Allows duplicate members.
 
 #Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
