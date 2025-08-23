3303c99611251529581d4216439083ff909f2cdc
taraksha <ashasumathi@gmail.com>
2025-08-23 16:32:38 +0900

    Started with Tuple

---
+++ src/data_types/tuple.py
@@ -0,0 +1,13 @@
+#TUPLE is a collection which is ordered and unchangeable. Allows duplicate members.()
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+
+#Single Tuple
+thistuple = ("apple",)
+print(type(thistuple))
