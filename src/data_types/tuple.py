189506b11a757fc25aa8405db585f836af4111a7
taraksha <ashasumathi@gmail.com>
2025-08-24 15:05:56 +0900

    Learnt various tuple operations

--- src/data_types/tuple.py
+++ src/data_types/tuple.py
@@ -4,10 +4,30 @@ thistuple = ("apple", "banana", "cherry")
 print(thistuple)
 
 
-#NOT a tuple
+#NOT a tuple , its a string
 thistuple = ("apple")
 print(type(thistuple))
 
 #Single Tuple
 thistuple = ("apple",)
 print(type(thistuple))
+
+#access : same as list.
+#Modification work around. Tuple->list(modify)->Tuple
+
+addTuple = ("banana",)
+thistuple += addTuple
+print(thistuple)
+
+#UNPACKING
+(red,yellow) = thistuple
+print(f"red is :{red},yellow is: {yellow}")
+
+unpack =(1,2,3,4,5,6,7,8,9,10)
+(num1,num2,*rest ) = unpack #unpacks the * variable with the rest of tuple into a list
+print(rest)
+print(type(rest))
+
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+print(mytuple)
