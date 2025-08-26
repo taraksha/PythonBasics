0d51679e45fa2137cc0622f9774f565220953e57
taraksha <ashasumathi@gmail.com>
2025-08-26 19:26:49 +0900

    Conditional operators If..else and Match

---
+++ src/conditional_operations.py
@@ -0,0 +1,31 @@
+#IF and ELSE
+a = 200
+b = 200
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
+
+#short hand
+if a > b: print("a is greater than b")
+print("A") if a > b else print("B")
+print("A") if a > b else print("=") if a == b else print("B")
+
+if b > a:
+    pass
+#to avoid error
+
+#MAtch - like switch (available from 3.10)
+"""
+month = 5
+day = 4
+match day:
+  case 1 | 2 | 3 | 4 | 5 if month == 4:
+    print("A weekday in April")
+  case 1 | 2 | 3 | 4 | 5 if month == 5:
+    print("A weekday in May")
+  case _:
+    print("No match")
+"""
