2b0b41db11b9ae02a62648ea55627e38ed431c48
taraksha <ashasumathi@gmail.com>
2025-09-01 14:22:02 +0900

    Learned about iterators for datatypes and class

---
+++ src/data_types/iterators.py
@@ -0,0 +1,26 @@
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+# You can provide custom iteration for your class using __iter__ and __next__
+class MyNumbers:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        if self.a <= 20:
+            x = self.a
+            self.a += 1
+            return x
+        else:
+            raise StopIteration
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+for x in myiter:
+    print(x)
