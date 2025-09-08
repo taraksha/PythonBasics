cf1ca942e0514014e38bf6cf948ea8d866213735
taraksha <ashasumathi@gmail.com>
2025-09-08 17:51:48 +0900

    Packages in Python.Started with math for mathematical operations

---
+++ src/python_library/math.py
@@ -0,0 +1,12 @@
+import math
+
+print(math.sqrt(100))#10
+print(math.pi)#3.141592653589793
+print(math.ceil(1.4)) # returns 2
+print(math.floor(1.4)) # returns 1
+
+#without imports
+print(min(2, 3, 53, 23, 5))#2
+print(max(2, 3, 53, 23, 5))#53
+print(abs(-87.34))#87.34
+print(pow(4, 2))#16
